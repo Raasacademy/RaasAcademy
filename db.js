ac54b4e34f93d3e5100ce9a3d755f9ce52008ea0
RuzzyTechnologies <rukkymukoro@gmail.com>
2024-11-20 01:06:49 +0100

    Edited the database connection

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 import mysql from 'mysql2';
 
 const db = mysql.createPool({
-  host: 'localhost',     // Database host, usually 'localhost' for local development
-  user: 'root', // Replace with your MySQL username
-  password: '', // Replace with your MySQL password
-  database: 'skillgro', // Replace with your database name
+  host: 'raasacademy-db.c3ce4yum6ngd.us-east-1.rds.amazonaws.com',     // Database host, usually 'localhost' for local development
+  user: 'raasacademy', // Replace with your MySQL username
+  password: 'raasacademy2024', // Replace with your MySQL password
+  database: 'raasacademy-db', // Replace with your database name
 });
 
 export default db;
